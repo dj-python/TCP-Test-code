eab252d145d21cfece918748c894e1a58383d9ff
dj-python <pdj911@naver.com>
2025-02-04 17:00:31 +0900

    Update PICO_TCP_Test.py

--- PICO_TCP_Test.py
+++ PICO_TCP_Test.py
@@ -8,35 +8,48 @@ class MainFW:
         # I2C 0,1번 초기화
 
         self.ipAddress = '166.79.26.140'
+        self.serverIpAddress = '166.79.26.142'
+        self.gateway = '166.79.26.1'
         self.portNumber = 6571
-
         self.rxMessage = str()
-        self.txMessage = str()
 
-        W5500.init(ipAddress=self.ipAddress, server_ip='166.79.26.142', gateway='166.79.26.1', server_port=6571)
+        W5500.init(ipAddress=self.ipAddress, server_ip=self.serverIpAddress, gateway=self.gateway, server_port=self.portNumber)
 
     def func_1msec(self):
         pass
 
     def func_10msec(self):
-        message = W5500.readMessage()
-        if message is not None:
-            self.rxMessage = message.decode('utf-8')
-            print(self.rxMessage)
-            self.sendback()
+        try:
+            message = W5500.readMessage()
+            if message is not None:
+                self.rxMessage = message.decode('utf-8')
+                print(self.rxMessage)
+                self.sendback()
+                self.rxMessage = str()                      # 무한 반복되는 것을 방지하기 위해 변수를 초기화
+        except Exception as e:
+            print(f"메시지 읽기 오류: {e}")
+
 
     def sendback(self):
-        W5500.sendMessage(f"데이터 수신에 성공했습니다. 수신한 데이터는 {self.rxMessage} 입니다.")
+        try:
+            W5500.sendMessage(f"데이터 수신에 성공했습니다. 수신한 데이터는 {self.rxMessage} 입니다.")
+
+        except Exception as e:
+            print(f"메시지 전송 오류: {e}")
+
 
 if __name__ == "__main__":
     cnt_msec = 0
     main = MainFW()
 
-    while True:
-        cnt_msec += 1
-        main.func_1msec()
+    try:
+        while True:
+            cnt_msec += 1
+            main.func_1msec()
 
-        if not cnt_msec % 10:
-            main.func_10msec()
+            if not cnt_msec % 10:
+                main.func_10msec()
 
-        time.sleep_ms(1)
+            time.sleep(0.001)
+    except KeyboardInterrupt:
+        print("프로그램 종료")
